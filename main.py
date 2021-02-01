101ba948f909d48069bf02c9d08bdd89c9b5d28b
portaljumper007 <enchantedportal007@gmail.com>
2021-02-01 15:02:50 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ from mpl_toolkits.mplot3d import axes3d, Axes3D
 
 class person():
     def newOpinion(self):
-        opinion = random.uniform(0,1)
+        opinion = 1 - (random.uniform(0,1) ** 20)
         certainty = random.uniform(0,1)
         return (opinion, certainty)
 
 ly = []
-for j in range(10000):
-    plt.clf()
+for j in range(1,10000):
     x = []
     y = []
     population = []
-    for i in range(1, 100, 1):
+    rang = np.linspace(0,1000,100)
+    for i in rang:
         population.append(person())
         opinions = []
         certainties = []
@@ -28,9 +28,10 @@ for j in range(10000):
         x.append(i)
         y.append(np.average(opinions, weights=certainties))
     ly.append(y)
-    rang = np.linspace(0,100,20)
-
-    for o in rang:
-        plt.plot(x, np.percentile(ly, o, axis=0))
-    plt.draw()
-    plt.pause(0.00001)
+    rang = np.linspace(0,100,15)
+    if j % 25 == 0:
+        plt.clf()
+        for o in rang:
+            plt.plot(x, np.percentile(ly, o, axis=0))
+        plt.draw()
+        plt.pause(0.0000001)
