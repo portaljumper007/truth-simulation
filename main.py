1cf8ca23b4e1e6a81fc22145819ad23b5d7c11c3
portaljumper007 <enchantedportal007@gmail.com>
2021-02-03 21:55:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import random
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d, Axes3D
 
+
 class person():
     def newOpinion(self):
         opinion = 1 - (random.uniform(0,1) ** 20)
@@ -12,7 +13,8 @@ class person():
         return (opinion, certainty)
 
 ly = []
-for j in range(1,10000):
+lx = []
+for j in range(1,100000000):
     x = []
     y = []
     population = []
@@ -27,11 +29,15 @@ for j in range(1,10000):
             certainties.append(c[1])
         x.append(i)
         y.append(np.average(opinions, weights=certainties))
+    #lx.extend(x)
+    #ly.extend(y)
     ly.append(y)
+    #lz = ly - np.average(ly, axis=0)
     rang = np.linspace(0,100,15)
-    if j % 25 == 0:
+    if j % 5 == 0:
         plt.clf()
+    #plt.tricontour(lx, ly, lz, 15, linewidths=0.5, colors='k')
         for o in rang:
             plt.plot(x, np.percentile(ly, o, axis=0))
-        plt.draw()
-        plt.pause(0.0000001)
+    plt.draw()
+    plt.pause(0.0000001)
